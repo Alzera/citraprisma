8b10d7ea168902a62d23cbdba90687e47003ec70
Alzera Cita <alzeracita23@gmail.com>
2022-07-30 22:18:50 +0700

    add auto build translation

---
+++ scripts/build-translation-loaders.mjs
@@ -0,0 +1,25 @@
+import fs from "fs";
+import glob from "glob";
+
+const langs = ['id','en']
+const folder = './src/routes/'
+const resultFile = './src/lib/translations/loaders.js'
+
+console.log('\x1b[1m\x1b[36m%s\x1b[0m', "> Using build-translation-loaders")
+glob(`${folder}**/*.+(svelte)`, function (er, files) {
+  let loaders = ""
+  files.map(i => i.replaceAll(folder, '').replaceAll('.svelte', '')).forEach(function (i) {
+    let
+      key = i.replaceAll('/', '.'),
+      routes = '/' + i.replaceAll('index', '').replaceAll(/(\/?_\S*$)/g, '')
+    if(routes != '/' && routes.endsWith('/'))
+      routes = routes.substring(0, routes.length - 1)
+    for(const l of langs){
+      const json = `./${l}/${i}.json`;
+      loaders += `{ locale: '${l}', key: '${key}', routes: ['${routes}'], loader: async () => (await import('${json}')).default, },`
+    }
+  })
+  loaders = `export default [${loaders}]`
+  fs.writeFileSync(resultFile, loaders);
+  console.log('\x1b[32m%s\x1b[0m', `  ✔ done file created on ${resultFile}`)
+})
