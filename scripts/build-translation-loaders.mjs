adbe489845d0bf967ab69cb9795e24851861c71c
Alzera Cita <alzeracita23@gmail.com>
2022-08-01 08:12:06 +0700

    Fix translation loader

--- scripts/build-translation-loaders.mjs
+++ scripts/build-translation-loaders.mjs
@@ -4,6 +4,7 @@ import glob from "glob";
 const langs = ['id','en']
 const folder = './src/routes/'
 const resultFile = './src/lib/translations/loaders.js'
+const underscoreRegex = /(\/?_\S*$)/g
 
 console.log('\x1b[1m\x1b[36m%s\x1b[0m', "> Using build-translation-loaders")
 glob(`${folder}**/*.+(svelte)`, function (er, files) {
@@ -11,12 +12,14 @@ glob(`${folder}**/*.+(svelte)`, function (er, files) {
   files.map(i => i.replaceAll(folder, '').replaceAll('.svelte', '')).forEach(function (i) {
     let
       key = i.replaceAll('/', '.'),
-      routes = '/' + i.replaceAll('index', '').replaceAll(/(\/?_\S*$)/g, '')
+      routes = '/' + i.replaceAll('index', '')
     if(routes != '/' && routes.endsWith('/'))
       routes = routes.substring(0, routes.length - 1)
+    if(underscoreRegex.test(routes)) routes = ''
+    if(routes != '') routes = `routes: ['${routes}'], `
     for(const l of langs){
       const json = `./${l}/${i}.json`;
-      loaders += `{ locale: '${l}', key: '${key}', routes: ['${routes}'], loader: async () => (await import('${json}')).default, },`
+      loaders += `{ locale: '${l}', key: '${key}', ${routes}loader: async () => (await import('${json}')).default, },`
     }
   })
   loaders = `export default [${loaders}]`
