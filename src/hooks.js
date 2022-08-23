7931ec9195403a70752e25a288a2810fa50cb5d0
Alzera Cita <alzeracita23@gmail.com>
2022-08-23 21:07:50 +0700

    Migrate to html-minifier-terser

---
+++ src/hooks.js
@@ -0,0 +1,34 @@
+import { minify } from 'html-minifier-terser';
+import { prerendering } from '$app/env';
+ 
+const minification_options = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+  decodeEntities: true,
+  html5: true,
+  minifyCSS: true,
+  minifyJS: true,
+  removeAttributeQuotes: true,
+  removeComments: true,
+  removeOptionalTags: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  sortAttributes: true,
+  sortClassName: true
+};
+ 
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+  const response = await resolve(event);
+ 
+  if (prerendering && response.headers.get('content-type') === 'text/html') {
+    return new Response(await minify(await response.text(), minification_options), {
+      status: response.status,
+      headers: response.headers
+    });
+  }
+ 
+  return response;
+}
